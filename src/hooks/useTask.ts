a3d56f69d005df4b0e66f8bf3e8cfc48ee5bdd63
jjlabrada <jjlabranate@gmail.com>
2025-04-20 09:18:57 -0400

    offline first feature

--- src/hooks/useTask.ts
+++ src/hooks/useTask.ts
@@ -6,22 +6,36 @@ export const useTask = () => {
   return useQuery({
     queryKey: ["tasks"],
     queryFn: () => fetchTasks(),
+    networkMode: "offlineFirst",
   });
 };
 
 export const useMutationTask = () => {
   const mutation = useMutation({
     mutationKey: ["tasks"],
-    mutationFn: (task: TaskEntry) => insertTask(task),
-    onSuccess: () => {
-      // Invalida la consulta de tareas para actualizar la lista automáticamente
+    mutationFn: (newTask: TaskEntry) => insertTask(newTask),
+    onMutate: (newTask) => {
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      const prevState = queryClient.getQueryData(["tasks"]) || [];
+      const optimisticValue = {
+        ...newTask,
+        id: `temp_${Date.now()}`, // temp
+      };
+      queryClient.setQueryData(["tasks"], [{ ...prevState }, optimisticValue]);
+      return { prevState };
     },
     onError: (_, __, context) => {
-      // Revertir en caso de error
-      console.log("Error al insertar tarea");
-      // if (context?.prev) queryClient.setQueryData(["tasks"], context.prev);
+      queryClient.setQueryData(["tasks"], context?.prevState);
     },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["tasks"], (old) =>
+        old.map((task) => (task.id.startsWith("temp_") ? data : task))
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    networkMode: "offlineFirst",
   });
   return mutation;
 };
