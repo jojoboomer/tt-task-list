45eb795aa637b094f243e7221906789e8d3fa177
jjlabrada <jjlabranate@gmail.com>
2025-04-21 00:37:22 -0400

    test: refactor tests to use QueryClientProvider and improve assertions

--- src/components/Task/Task.test.tsx
+++ src/components/Task/Task.test.tsx
@@ -1,3 +1,5 @@
+import { queryClient } from "@/lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, describe, it } from "vitest";
 import { Task } from "./Task";
@@ -6,7 +8,7 @@ describe("Task", () => {
   afterEach(cleanup);
 
   const data: Task = {
-    id: 1,
+    id: "1",
     title:
       "#Important Write to some_email@gmail.com and tell @natasha about https://staging.alldone.app",
     status: "pending",
@@ -14,8 +16,12 @@ describe("Task", () => {
   };
 
   it("should render", () => {
-    render(<Task data={data} />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Task data={data} />
+      </QueryClientProvider>
+    );
 
-    screen.getByText("natasha", {exact: false})
+    screen.getByText("natasha", { exact: false });
   });
 });
--- src/tests/integration/App.test.tsx
+++ src/tests/integration/App.test.tsx
@@ -1,15 +1,20 @@
-// App.test.tsx
 import App from "@/App";
 import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { afterEach, describe, expect, it } from "vitest";
 
-describe("Integration test for App", () => {
+describe("Integration test for App",  () => {
   afterEach(cleanup);
 
-  it("should render", () => {
+  it("should render", async () => {
     render(<App />);
+
+    const task = screen.getByTestId("new-task");
+    await userEvent.click(task);
+
     screen.getByRole("main");
     screen.getByRole("list");
     expect(screen.getAllByTestId("new-task")).toHaveLength(1);
+
   });
 });
